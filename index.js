c8b14cfc0d2fcf76b37d458ce21330858f1a94b2
Sayanduary <sayanduary@gmail.com>
2025-02-25 20:26:30 +0530

    done

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ const hostRouter = require('./routes/hostRouter');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(req.url, req.method);
-  next();
-})
+
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(userRouter);
-app.use(hostRouter);
+app.use('/host', userRouter);
+app.use('/host', hostRouter);
 
-
+app.use((req, res) => {
+  res.status(404).send(`<h1>Your Page is not found</h1>`)
+})
 
 
 const PORT = 3001;
--- routes/hostRouter.js
+++ routes/hostRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hostRouter = express.Router();
 
-hostRouter.get('/host/add-home', (req, res, next) => {
+hostRouter.get('/add-home', (req, res, next) => {
   res.send(`<h1>Register Your Home Here<h1>
     
       <form action="/host/add-home" method="POST">
@@ -12,7 +12,7 @@ hostRouter.get('/host/add-home', (req, res, next) => {
 });
 
 
-hostRouter.post('/host/add-home', (req, res, next) => {
+hostRouter.post('/add-home', (req, res, next) => {
   console.log(req.body);
   res.send(`<h1>Home Registered Succesfully<h1>
     
