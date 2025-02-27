00f75ce5d10d6e7079eab0191d09a085f972208f
Sayanduary <sayanduary@gmail.com>
2025-02-27 19:26:29 +0530

    done

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const userRouter = require('./routes/userRouter');
 
 const hostRouter = require('./routes/hostRouter');
 
+const rootDir = require('./utils/pathUtil');
+
 const app = express();
 
 
@@ -18,7 +20,7 @@ app.use(userRouter);
 app.use('/host', hostRouter);
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404page.html'));
+  res.status(404).sendFile(path.join(rootDir, 'views', '404page.html'));
 });
 
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const userRouter = express.Router();
-
+const rootDir = require('../utils/pathUtil')
 //Core Modules
 
 const path = require('path');
 
 userRouter.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname,'../','views','home.html'));
+  res.sendFile(path.join(rootDir, 'views', 'home.html'));
 });
 
 module.exports = userRouter;
